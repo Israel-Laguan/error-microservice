a691a53847464807ccc0e55662daec5d0b8b531a
Israel Antonio Rosales Laguan <israellaguan@gmail.com>
2022-09-21 00:53:38 -0500

    feat: add status dependency

--- src/routes/api/status.rs
+++ src/routes/api/status.rs
@@ -3,8 +3,11 @@ use thruster::{m, App};
 
 mod controllers;
 
-use controllers::am_i_up;
+use controllers::{about, am_i_up, dependency};
 
 pub fn status_app() -> App<HyperRequest, Ctx, ()> {
-    App::<HyperRequest, Ctx, ()>::create(generate_context, ()).get("/am-i-up", m![am_i_up])
+    App::<HyperRequest, Ctx, ()>::create(generate_context, ())
+        .get("/about", m![about])
+        .get("/am-i-up", m![am_i_up])
+        .get("/:dependency", m![dependency])
 }
--- src/routes/api/status/controllers.rs
+++ src/routes/api/status/controllers.rs
@@ -1,11 +1,40 @@
-use hyper::header;
+use std::env;
+
+use serde_json::json;
 use thruster::context::basic_hyper_context::BasicHyperContext as Ctx;
-use thruster::{middleware, Context, MiddlewareNext, MiddlewareResult};
+use thruster::{middleware, MiddlewareNext, MiddlewareResult};
+
+#[middleware]
+pub async fn about(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    const ID: &str = env!("CARGO_PKG_NAME");
+    const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
+    let response: &str = &json!({
+        "id": ID,
+        "description": DESCRIPTION,
+        "version": VERSION,
+    })
+    .to_string();
+    context.status(200);
+    context.content_type("application/json");
+    context.body(response);
+    Ok(context)
+}
 
 #[middleware]
 pub async fn am_i_up(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
     context.status(200);
-    context.set(header::CONTENT_TYPE.as_ref(), "application/text");
+    context.content_type("application/text");
+    context.body("OK");
+    Ok(context)
+}
+
+#[middleware]
+pub async fn dependency(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    let dependency = context.hyper_request.as_ref().unwrap().params.get("dependency").unwrap().param.clone();
+    log::info!("{}", dependency);
+    context.status(200);
+    context.content_type("application/text");
     context.body("OK");
     Ok(context)
 }
